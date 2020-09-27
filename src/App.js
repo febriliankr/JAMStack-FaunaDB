d5ed3ebcae831caa5bbb0992c37749c350ee2c3f
febriliankr <febrilian.kr@gmail.com>
2020-09-27 10:19:52 +0700

    added loading animation with react-loadingg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import LinkForm from "./components/LinkForm";
 import LinkList from "./components/LinkList";
+import { CircleToBlockLoading  } from "react-loadingg";
 
 //grab all the links
 //display all the links
 //add delete and archive functionality
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [links, setLinks] = useState([]);
 
   const loadLinks = async () => {
@@ -14,9 +16,10 @@ function App() {
       const res = await fetch("/.netlify/functions/getLinks");
       const links = await res.json();
       setLinks(links);
-      console.log('links', links)
-    }catch(err){
-      console.error('error: ', err);
+      console.log("links", links);
+      setLoading(false);
+    } catch (err) {
+      console.error("error: ", err);
     }
   };
 
@@ -27,10 +30,16 @@ function App() {
   return (
     <div className="container">
       <h1>List of Links</h1>
-      <LinkForm refreshLinks={loadLinks}/>
+
       <p>These are all the links that I frequently use.</p>
-      <LinkList links={links} refreshLinks={loadLinks}/>
-      
+      {loading ? (
+        <CircleToBlockLoading  />
+      ) : (
+        <>
+          <LinkForm refreshLinks={loadLinks} />
+          <LinkList links={links} refreshLinks={loadLinks} />
+        </>
+      )}
     </div>
   );
 }
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,22 +1,37 @@
 import React from "react";
 import LinkCard from "./LinkCard";
 
+
 export default function LinkList({ links, refreshLinks }) {
   return (
     <div>
       {links &&
-        links.filter(link => !link.archived).map((link) => {
-          return (
-            <LinkCard key={link._id} link={link} refreshLinks={refreshLinks} />
-          );
-        })}
+        links
+          .filter((link) => !link.archived)
+          .map((link) => {
+            return (
+              <LinkCard
+                key={link._id}
+                link={link}
+                refreshLinks={refreshLinks}
+              />
+            );
+          })}
+          
+
       <h2>Archived Links</h2>
       {links &&
-        links.filter(link => link.archived).map((link) => {
-          return (
-            <LinkCard key={link._id} link={link} refreshLinks={refreshLinks} />
-          );
-        })}
+        links
+          .filter((link) => link.archived)
+          .map((link) => {
+            return (
+              <LinkCard
+                key={link._id}
+                link={link}
+                refreshLinks={refreshLinks}
+              />
+            );
+          })}
     </div>
   );
 }
